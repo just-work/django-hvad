6d904c968953c0838b4ab7d8793d61b985a0d291
ivasonn <ivasonn@gmail.com>
2016-11-02 21:06:11 +0300

    #add flag to raise exception when translation does not exist

--- hvad/descriptors.py
+++ hvad/descriptors.py
@@ -1,9 +1,13 @@
 import django
+from django.conf import settings
 from django.db.models.fields import FieldDoesNotExist
 from django.utils.translation import get_language
 from hvad.utils import get_translation, set_cached_translation, get_cached_translation
 if django.VERSION >= (1, 7):
     from django.apps import registry
+
+RAISE_IF_NOT_TRANSLATION = getattr(settings, 'RAISE_IF_NOT_TRANSLATION', True)
+
 
 class BaseDescriptor(object):
     """
@@ -11,20 +15,23 @@ class BaseDescriptor(object):
     """
     def __init__(self, opts):
         self.opts = opts
-        self._NoTranslationError = type('NoTranslationError',
-                                        (AttributeError, opts.translations_model.DoesNotExist),
-                                        {})
-    
+        self._NoTranslationError = type(
+            'NoTranslationError', (
+                AttributeError, opts.translations_model.DoesNotExist), {})
+
     def translation(self, instance):
         translation = get_cached_translation(instance)
         if translation is None:
             try:
                 translation = get_translation(instance)
             except self.opts.translations_model.DoesNotExist:
-                raise self._NoTranslationError('Accessing a translated field requires that '
-                                               'the instance has a translation loaded, or a '
-                                               'valid translation in current language (%s) '
-                                               'loadable from the database' % get_language())
+                if RAISE_IF_NOT_TRANSLATION:
+                    raise self._NoTranslationError(
+                        'Accessing a translated field requires that the '
+                        'instance has a translation loaded, or a valid '
+                        'translation in current language (%s) loadable from '
+                        'the database' % get_language())
+                translation = get_translation(instance, settings.LANGUAGE_CODE)
             set_cached_translation(instance, translation)
         return translation
 
@@ -49,10 +56,10 @@ class TranslatedAttribute(BaseDescriptor):
             except FieldDoesNotExist as e: #pragma: no cover (django 1.6 before models loaded)
                 raise AttributeError(*e.args)
         return getattr(self.translation(instance), self.name)
-    
+
     def __set__(self, instance, value):
         setattr(self.translation(instance), self.name, value)
-    
+
     def __delete__(self, instance):
         delattr(self.translation(instance), self.name)
 
@@ -61,14 +68,14 @@ class LanguageCodeAttribute(TranslatedAttribute):
     """
     The language_code attribute is different from other attribtues as it cannot
     be deleted. Trying to do so will always cause an attribute error.
-    
+
     """
     def __init__(self, opts):
         super(LanguageCodeAttribute, self).__init__(opts, 'language_code')
-    
+
     def __set__(self, instance, value):
         raise AttributeError("The 'language_code' attribute cannot be "
                              "changed directly! Use the translate() method instead.")
-    
+
     def __delete__(self, instance):
         raise AttributeError("The 'language_code' attribute cannot be deleted!")
